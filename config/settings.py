5df7f9ce7979effc361195d41e4df33160312fbe
Andres Villca <andres24vt@gmail.com>
2026-05-09 16:20:31 +0200

    Settings

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +75,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django_mongodb_backend',
-        'HOST': 'mongodb://localhost:27017/',
-        'NAME': 'config',
-    },
+    "default": {
+        "ENGINE": "django_mongodb_backend",
+        "HOST": os.getenv("MONGODB_URI"),
+        "NAME": os.getenv("MONGODB_NAME"),
+    }
 }
 
 # Database routers
